922f33e0f625ce06be290fa1fcaecfb6588e72a1
KozaX <55134615+KozaX@users.noreply.github.com>
2020-07-09 07:37:42 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,31 +1,52 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
   //... your code goes here
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+  const subtotalEl = product.querySelector('.subtotal span');
+
+  const priceAmount = price.innerHTML;
+  const quantityAmount = quantity.value;
+
+  const subtotalAmount = parseFloat(priceAmount) * parseInt(quantityAmount);
+
+  subtotalEl.innerHTML = subtotalAmount;
+
+  return subtotalAmount;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  const totalArr = [];
+  const products = document.querySelectorAll('.product');
+  const totalAmountEl = document.querySelector('#total-value span');
 
-  // ITERATION 2
-  //... your code goes here
+  for (let el of products) {
+    totalArr.push(updateSubtotal(el));
+  }
 
-  // ITERATION 3
-  //... your code goes here
+  const totalAmount = totalArr.reduce((acc, val) => {
+    return acc + val;
+  }, 0);
+
+  totalAmountEl.innerHTML = totalAmount;
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const target = event.target.parentElement.parentElement.parentElement;
+  const childToRemove = event.target.parentElement.parentElement;
+  const amountToRemove = childToRemove.querySelector('.subtotal span')
+    .innerHTML;
+  let totalAmountEl = document.querySelector('#total-value span');
+
+  console.log(totalAmountEl);
+
+  target.removeChild(childToRemove);
+
+  totalAmountEl.innerHTML =
+    parseFloat(totalAmountEl.innerHTML) - parseFloat(amountToRemove);
 }
 
 // ITERATION 5
@@ -36,6 +57,12 @@ function createProduct() {
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
+  const removeBtns = document.querySelectorAll('.action .btn');
+
+  for (let el of removeBtns) {
+    el.addEventListener('click', removeProduct);
+  }
+
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   //... your code goes here
